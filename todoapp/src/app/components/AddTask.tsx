f31926e0016a74fa7dbdd243bed81298408f91f5
tt1125 <tsuru.takeshi275@gmail.com>
2023-12-26 23:51:08 +0900

    一旦

---
+++ todoapp/src/app/components/AddTask.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AddTask = () => {
+  return (
+    <div></div>
+  )
+}
+
+export default AddTask
---
+++ todoapp/src/app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>to do app with Next.js</h1>
+     <div><div>
+      <AddTask/>
+      <ToDoList/>
+      </div></div>
+
+    </main>
+  )
+}
